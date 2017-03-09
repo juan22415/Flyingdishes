c42e02a36b6bd03195706c2bb973988f1743b180
juan camilo rios <juan22415@hotmail.com>
2017-03-08 19:16:22 -0500

    tap conroller

--- Flyingdishes/Assets/character_move.cs
+++ Flyingdishes/Assets/character_move.cs
@@ -16,8 +16,37 @@ public class character_move : MonoBehaviour {
 
 	void Update () {
 
-        float horizontalmove = Input.GetAxis("Horizontal");
-        move(horizontalmove);
+        
+
+        if (Input.touchCount>0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.position.x > Screen.width / 2)
+            {
+
+
+                char_rigidbody.velocity = new Vector2(1 * speed, 0);
+            }
+
+            else if (touch.position.x < Screen.width / 2)
+            {
+
+                Debug.Log("touchside2");
+                char_rigidbody.velocity = new Vector2(-1 * speed, 0);
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+                char_rigidbody.velocity = Vector2.zero;
+        }
+
+
+
+
+
+
+        //float horizontalmove = Input.GetAxis("Horizontal");
+       // move(horizontalmove);
 		
 	}
 
@@ -25,4 +54,7 @@ public class character_move : MonoBehaviour {
     {   
         char_rigidbody.velocity=new Vector2(hmove * speed, 0);
     }
+
+   
+
 }
