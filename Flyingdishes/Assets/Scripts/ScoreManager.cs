d7f00d915a7c51ada923aecb5ea1ae819dc907ee
Manuela Recio <manu138@hotmail.es>
2017-03-29 12:31:50 -0500

    Agregue el singleton generico, además el score.

---
+++ Flyingdishes/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager :  Singleton<ScoreManager>
+{
+    public Text scoreText;
+    public int score = 0;
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        scoreText.text = "Score:" + score;
+    }
+}
